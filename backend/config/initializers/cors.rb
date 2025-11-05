05639ec3f9118b1bda2ee72ad27fde667e012e80
Diti <dn24002@edu.lu.lv>
2025-11-05 20:38:08 +0200

    Update CORS origins for the application

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://wimmelbild.netlify.app/"
+    origins "https://benevolent-crostata-059d4e.netlify.app/"
     resource "*", headers: :any, methods: [:get, :post, :options]
   end
 end
