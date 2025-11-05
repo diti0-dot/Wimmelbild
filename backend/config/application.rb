bcca4c7888a5eee9f48d689e1095f04020f2f4b1
diti <diti@diti.localdomain>
2025-11-05 21:15:09 +0200

    jhasg

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,22 +1,4 @@
-require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
@@ -40,22 +22,4 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # CORS configuration - ADD THIS SECTION
-   config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://benevolent-crostata-059d4e.netlify.app'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Access-Control-Allow-Origin'],
-      max_age: 600
-  end
-end
-
-    # Add back cookies and session middleware if needed
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
-  end
 end
